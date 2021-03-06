e42ba4832caef3ffce56d4fcd5bb8ab013701ffc
Philippe VK <cybr3monk@pm.me>
2021-03-06 00:26:41 -0500

    (wip) publish.sh

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+set -ueo pipefail
+VERSION="v$(node -e 'console.log(require("./manifest.json").version)')"
+
+if [ "$(git branch --show-current)" != "master" ]; then
+    echo "publish can only run on master branch";
+    exit 1;
+fi
+
+if [ -z "$(git status --porcelain)" ]; then
+    echo "git has uncommitted changes";
+    exit 1;
+fi
+
+if git rev-list "$VERSION" &>/dev/null; then
+    echo "tag $VERSION already exists";
+    exit 1;
+fi
+
+echo "version: $VERSION"
