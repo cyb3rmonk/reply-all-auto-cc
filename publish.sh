b7202f91b5fc0caeed8371b3b66f28d1bf71c02c
Philippe VK <cybr3monk@pm.me>
2021-03-06 00:49:53 -0500

    (wip) publish.sh

--- publish.sh
+++ publish.sh
@@ -1,22 +1,28 @@
 #! /bin/bash
 set -ueo pipefail
-VERSION="v$(node -e 'console.log(require("./manifest.json").version)')"
+NAME="$(node -e 'console.log(require("./manifest.json").name)')"
+VERSION="$(node -e 'console.log(require("./manifest.json").version)')"
 
-if [ "$(git branch --show-current)" != "master" ]; then
-    echo "publish can only run on master branch";
-    exit 1;
+# sanity checks (skipped with --force flag)
+
+if [ "${1:-}" != "--force" ]; then
+    if [ "$(git branch --show-current)" != "master" ]; then
+        echo "publish can only run on master branch";
+        exit 1;
+    fi
+    GIT_STATUS="$(git status --porcelain)";
+    if [ "$GIT_STATUS" ]; then
+        echo "git has uncommitted changes:";
+        echo "$GIT_STATUS";
+        exit 1;
+    fi
+    if git rev-list "v$VERSION" &>/dev/null; then
+        echo "tag $VERSION already exists";
+        exit 1;
+    fi
 fi
 
-GIT_STATUS="$(git status --porcelain)";
-if [ "$GIT_STATUS" ]; then
-    echo "git has uncommitted changes:";
-    echo "$GIT_STATUS";
-    exit 1;
-fi
-
-if git rev-list "$VERSION" &>/dev/null; then
-    echo "tag $VERSION already exists";
-    exit 1;
-fi
-
-echo "version: $VERSION"
+# publishing
+echo "publishing $NAME $VERSION...";
+FILENAME="${NAME}_${VERSION}"
+zip "./$FILENAME.xpi" ./manifest.json ./*.js
